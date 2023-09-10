9d7b578510ea4b4419fe699c120f737c96658730
“your <alexnelson97@gmail.com>
2023-09-10 15:45:19 -0600

    pushing answers to sdlc.js

--- sdlc.js
+++ sdlc.js
@@ -4,18 +4,34 @@
 /*
     Reorganize the following steps of the Software Development Lifecycle to be in their correct order.
 */
+// These have been switched to the correct order
 
-// Implementation
-// Design
-// Maintain
-// Analysis of Requirements
-// Testing & Integration
 // Planning
-
+// Analysis of Requirements
+// Design
+// Implementation
+// Testing & Integration
+// Maintain
 
 //////////////////PROBLEM 2////////////////////
 /*
     Create a variable for each of the above steps of the Software Development Lifecycle and set the values of each variable to a definition of the step.
     The first one is done for you. 
 */
-const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added." 
+const planning =
+  "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added.";
+
+const analysisOfRequirements =
+  "In this step, a detailed analysis of the project's requirements is performed. This includes gathering and documenting all requirements of the project.";
+
+const design =
+  "During the design phase, the architectural and structural aspects of the software are planned. This includes creating system designs, user interface designs, and any technical specifications necessary for implementation.";
+
+const implementation =
+  "Implementation involves the actual coding and development of the software based on the designs and requirements defined in earlier steps. Developers write the source code, test it, and integrate it into the system.";
+
+const testingAndIntegration =
+  "This step focuses on quality assurance and ensuring that the software functions as intended. Testing is performed to identify and fix any defects, and integration involves combining different components into a cohesive whole.";
+
+const maintain =
+  "Maintenance is an ongoing phase where the software is regularly updated, enhanced, and monitored for any issues. This phase ensures that the software continues to meet the evolving needs of users and remains reliable and secure.";
